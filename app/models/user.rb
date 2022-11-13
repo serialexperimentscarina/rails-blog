8b3817c842bede5be58f676e9ff266ece6a8b332
Carina <hiicariina@gmail.com>
2022-11-13 15:27:27 -0300

    Add dependency between user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          #User has many posts
-         has_many :posts
+         has_many :posts, dependent: :destroy
 end
