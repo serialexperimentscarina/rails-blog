53843e2dc8580d6ccf541b772da70b281256dfcf
Carina <hiicariina@gmail.com>
2022-11-14 20:47:06 -0300

    Show posts by most recent

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json
